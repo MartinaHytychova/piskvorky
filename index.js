20d6283dac445e05d9cd97d1f9c16de01e25aed9
Martina Hytychová <martina.hyt@gmail.com>
2020-10-22 21:03:30 +0200

    střídání hráčů, průběh hry

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//tvorba hrací plochy
 const BtnGrid = () => {
   let elem = '';
   let i = 0;
@@ -10,5 +11,43 @@ const BtnGrid = () => {
   return elem;
 };
 
+const InnerElm = () => {
+  return '<div class="inner"></div>';
+};
+
 const btnElm = document.querySelector('#grid');
-btnElm.innerHTML = BtnGrid();
+btnElm.innerHTML = InnerElm() + BtnGrid();
+
+//kdo je na tahu
+let player = 'circle';
+
+const SvgElm = () => {
+  return `<img class="image__svg" src="./images/${player}.svg"/>`;
+};
+
+const round = () => {
+  const playerElm = document.querySelector('#player');
+  playerElm.innerHTML = SvgElm();
+};
+
+// hra
+const play = () => {
+  const buttons = document.querySelectorAll('.grid-item');
+  for (let j = 0; j < buttons.length; j++) {
+    const button = buttons[j];
+    button.addEventListener('click', () => {
+      if (button.classList !== `board__field--${player}`) {
+        button.classList.add(`board__field--${player}`);
+        if (player === 'cross') {
+          player = 'circle';
+          round();
+        } else {
+          player = 'cross';
+          round();
+        }
+        button.setAttribute('disabled', true);
+      }
+    });
+  }
+};
+play();
