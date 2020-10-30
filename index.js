30966274bff1822c3e1778cc482f4c3c99a9f503
Martina Hytychová <martina.hyt@gmail.com>
2020-10-31 00:34:29 +0100

    určení vítěze

--- index.js
+++ index.js
@@ -40,24 +40,147 @@ const playerTurn = () => {
   playerElm.innerHTML = SvgElm(player);
 };
 
+const buttons = document.querySelectorAll('.grid-item');
 // hra
 const play = () => {
-  const buttons = document.querySelectorAll('.grid-item');
   for (let j = 0; j < buttons.length; j++) {
     const button = buttons[j];
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
       if (button.classList !== `board__field--${player}`) {
         button.classList.add(`board__field--${player}`);
         if (player === 'cross') {
+          if (isWinningMove(e.target)) {
+            isWinningMove(e.target);
+            alert(`Vyhrál ${player}`);
+          }
           player = 'circle';
           playerTurn();
         } else {
+          if (isWinningMove(e.target)) {
+            isWinningMove(e.target);
+            if (confirm(`Vyhrál ${player}`)) {
+              location.reload();
+            }
+          }
           player = 'cross';
           playerTurn();
         }
         button.setAttribute('disabled', true);
       }
+      diagonalCheck(e.target);
     });
   }
 };
 play();
+
+// hrací plocha
+const boardSize = 10;
+const fields = document.querySelectorAll('.grid-item');
+
+//získej pozici z hrací plochy
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length) {
+    if (field === fields[fieldIndex]) {
+      break;
+    }
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+// získej políčko v řádku a sloupci
+const getField = (row, column) => fields[row * boardSize + column];
+
+// získej symbol hráče
+const getSymbol = (field) => {
+  if (field.classList.contains('board__field--cross')) {
+    return 'cross';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'circle';
+  }
+};
+
+// ověření, zda je 5 stejných symbolů vedle sebe
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1; // Jednička pro právě vybrané políčko
+  // check doleva
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  // check doprava
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  // check nahoru
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  // check dolu
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
+
+// nová hrací plocha
+/*
+const arrBoard = [];
+for (let i = 0; i < boardSize; i++) {
+  const newRow = [];
+  for (let j = 0; j < boardSize; j++) {
+    newRow.push(buttons[i * boardSize + j]);
+  }
+  arrBoard.push(newRow);
+}
+
+// check diagonálně
+const diagonalCheck = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+  const newOrigin = { row: origin.row, column: origin.column };
+
+  for (let i = 1; i < symbolsToWin; i++) {
+    newOrigin.row = i;
+    newOrigin.column = i;
+    const newSymbol = getSymbol(arrBoard[origin.row + i][origin.column + i]);
+    console.log(newSymbol);
+  }
+};*/
